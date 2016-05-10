f88cb0f106ecd71ebd6cf3fece5abf5d67c0b8c1
Leonardo Neves <enfimleo@gmail.com>
2016-05-10 00:46:08 -0300

    rotacao 80% + problema movimento + problema tiro

--- Assets/moveSpeed.cs
+++ Assets/moveSpeed.cs
@@ -3,23 +3,33 @@ using System.Collections;
 
 public class moveSpeed : MonoBehaviour {
 
-    public float velocidade = 0.001f;
+    public float speed = 0.001f;
     public GameObject ship;
     public GameObject camera;
+    Vector3 impulse;
 
-    // Use this for initialization
     void Start () {
-	
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.LeftControl))
-            //this.transform.position -= velocidade * transform.forward;
-            ship.AddForce(transform.forward * velocidade);
+        impulse = new Vector3(0, 0, speed);
 
-        if (Input.GetKey(KeyCode.LeftShift))
-            //this.transform.position += velocidade * transform.forward;
-            ship.AddForce(-1 * transform.forward * velocidade);
+        if (Input.GetKey(KeyCode.LeftControl)) {
+            //this.transform.position -= speed * transform.forward;
+            //ship.GetComponent<Rigidbody>().AddForce(-impulse);
+            //speed -= 5; 
+            ship.transform.position -= impulse;
+        }
+            
+
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            //this.transform.position += speed * transform.forward;
+            //ship.GetComponent<Rigidbody>().AddForce(impulse);
+            // speed += 5;
+            ship.transform.position += impulse;
+        }
+           
     }
 }
